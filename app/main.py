685321c00b02943812e5398fdda462e4f5f1f40a
020210210 <rudikalexx88@gmail.com>
2025-03-10 23:20:31 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, jsonify
+import random
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "Hello, World!"
+
+@app.route('/heartbeat')
+def heartbeat():
+    heart_rate = random.randint(60, 120)
+    return jsonify({"heart_rate": heart_rate})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
