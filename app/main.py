33f204339810bb134f940a311ab7c3aa70452e9a
020210210 <rudikalexx88@gmail.com>
2025-03-11 10:49:18 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -22,3 +22,18 @@ signal.signal(signal.SIGINT, handle_exit)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return '<h1>Добро пожаловать в IntuitivePulseApp!</h1>'
+
+@app.route('/about')
+def about():
+    return '<p>Это тестовая страница о проекте.</p>'
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=8000, debug=True)
