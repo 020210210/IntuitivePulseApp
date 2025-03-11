6e31051df248b81862662d7e2ff0e7aefa600bb6
020210210 <rudikalexx88@gmail.com>
2025-03-11 10:57:36 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,28 +1,3 @@
-import signal
-import sys
-from flask import Flask, jsonify
-import random
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "Hello, World!"
-
-@app.route('/heartbeat')
-def heartbeat():
-    heart_rate = random.randint(60, 120)
-    return jsonify({"heart_rate": heart_rate})
-
-def handle_exit(signum, frame):
-    print("Shutting down server gracefully...")
-    sys.exit(0)
-
-signal.signal(signal.SIGINT, handle_exit)
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
-
 from flask import Flask
 
 app = Flask(__name__)
