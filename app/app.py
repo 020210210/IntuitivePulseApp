238d38d4ec47af049c6cc7bd1fdb10f78ee61033
020210210 <rudikalexx88@gmail.com>
2025-03-11 11:43:00 +0200

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,16 +1,49 @@
-from flask import Flask, jsonify
-import random
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_socketio import SocketIO, emit
+import os
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sos_app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+socketio = SocketIO(app, cors_allowed_origins="*")
 
-@app.route('/')
-def home():
-    return "Hello, World!"
+# Модель пользователя
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), nullable=False)
+    latitude = db.Column(db.Float, nullable=True)
+    longitude = db.Column(db.Float, nullable=True)
 
-@app.route('/heartbeat')
-def heartbeat():
-    heart_rate = random.randint(60, 120)
-    return jsonify({"heart_rate": heart_rate})
+# Создание БД
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
+# Эндпоинт для SOS-сигнала
+@app.route('/sos', methods=['POST'])
+def sos():
+    data = request.json
+    user = User.query.filter_by(username=data['username']).first()
+    
+    if not user:
+        user = User(username=data['username'], latitude=data['latitude'], longitude=data['longitude'])
+        db.session.add(user)
+    else:
+        user.latitude = data['latitude']
+        user.longitude = data['longitude']
+    
+    db.session.commit()
+    socketio.emit('update_location', {'username': user.username, 'latitude': user.latitude, 'longitude': user.longitude})
+    
+    return jsonify({"message": "SOS сигнал отправлен!"})
+
+# Событие для чата
+@socketio.on('send_message')
+def handle_message(data):
+    emit('receive_message', data, broadcast=True)
+
+# Запуск сервера
+if __name__ == '__main__':
+    socketio.run(app, host='0.0.0.0', port=8000, debug=True)
